5eff476a2390d455217b60977108e7507aca9f12
Vincent <vincent.tn.le@gmail.com>
2021-05-31 21:36:37 -0400

    search api test

--- api/api.py
+++ api/api.py
@@ -1,13 +1,18 @@
 import time
 from flask import Flask
 from flask_cors import CORS
+import pandas as pd
 
 app = Flask(__name__)
 cors = CORS(app)
 
 @app.route("/search/<searchInput>")
 def getData(searchInput):
-  return time.time()
+  #\ufffd is space
+  searchInput = searchInput.replace("_", "\ufffd")
+  df = pd.read_csv('test.csv')
+  search_df = df[df['Content Desc'] == searchInput]
+  return {'ProductName': searchInput, 'Price': list(search_df['Price']), 'Date': list(search_df['Date']), "Test": list(df['Price'])} 
 
 @app.route("/email/<emailInput>")
 def postEmail(email):
