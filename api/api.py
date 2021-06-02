e6279ca4464cc7e9eaa008393d6e45c34914d64d
ChristineAu-yeung <auye4760@mylaurier.ca>
2021-06-02 19:14:28 -0400

    Updated api.py and added Store column to test.csv

--- api/api.py
+++ api/api.py
@@ -6,20 +6,17 @@ import pandas as pd
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.route("/search/<searchInput>")
-def getData(searchInput):
-  df = pd.read_csv("test.csv")
-  searchInput = searchInput.replace("_", " ")
-  search_df = df[df["Content Desc"] == searchInput]
-  return {"ProductName":searchInput, "Prices": list(search_df["Price"]), "Dates":list(search_df["Date"])}
 
-@app.route("/table/<searchInput>")
-def getData(searchInput):
-  df = pd.read_csv("test.csv")
-  searchInput = searchInput.replace("_", " ")
-  search_df = df[df["Content Desc"] == searchInput]
-  return {"ProductName":searchInput, "x": list(search_df["Price"]), "y":list(search_df["Date"])}
+@app.route("/search/<searchInput>")
+def getTableData(searchInput):
+    df = pd.read_csv("test.csv")
+
+
+@app.route("/table/<productID>")
+def getGraphData(productID):
+    df = pd.read_csv("test.csv")
+
 
 @app.route("/email/<emailInput>")
 def postEmail(email):
-  print("hi")
+    print("hi")
