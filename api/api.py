d928253ec8b1b129a883add8a7002a1cfb975a26
Felicia Jiang <fljiang@edu.uwaterloo.ca>
2021-06-03 18:31:40 -0400

    Indent

--- api/api.py
+++ api/api.py
@@ -7,37 +7,37 @@ cors = CORS(app)
 
 @app.route("/table/<searchInput>")
 def getTableData(searchInput):
-    df = pd.read_csv("test.csv")
-    return {
-      "data": [
-        {
-          "id": 1,
-          "name": searchInput,
-          "store": "A",
-          "stock": "Y",
-          "price": 1
-        },
-        {
-          "id": 2,
-          "name": searchInput,
-          "store": "B",
-          "stock": "N",
-          "price": 2
-        },
-        {
-          "id": 3,
-          "name": searchInput,
-          "store": "C",
-          "stock": "Y",
-          "price": 3
-        }
-      ]
-    }
+  df = pd.read_csv("test.csv")
+  return {
+    "data": [
+      {
+        "id": 1,
+        "name": searchInput,
+        "store": "A",
+        "stock": "Y",
+        "price": 1
+      },
+      {
+        "id": 2,
+        "name": searchInput,
+        "store": "B",
+        "stock": "N",
+        "price": 2
+      },
+      {
+        "id": 3,
+        "name": searchInput,
+        "store": "C",
+        "stock": "Y",
+        "price": 3
+      }
+    ]
+  }
 
 @app.route("/graph/<productID>")
 def getGraphData(productID):
-    df = pd.read_csv("test.csv")
+  df = pd.read_csv("test.csv")
 
 @app.route("/email/<emailInput>")
 def postEmail(email):
-    print("hi")
+  print("hi")
