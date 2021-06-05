3b0a06cb3b1baac4966d2b97dae4fa3f574c9a8b
ChristineAu-yeung <auye4760@mylaurier.ca>
2021-06-04 21:53:16 -0400

    Updated indentation to spaces

--- api/app.py
+++ api/app.py
@@ -10,27 +10,27 @@ list = []
 
 @app.route("/table/<searchInput>")
 def getTableData(searchInput):
-	df = pd.read_csv("test.csv")
-	tableDataDf = df.loc[df['name'] == searchInput]
-	tableDataDf = tableDataDf.drop_duplicates(subset=['id'], keep='last')
-	result = tableDataDf.to_json(orient="table")
-	parsed = json.loads(result)
-	for i in parsed["data"]:
-		dict = {'name': i["name"], 'id': i['id'], 'store':i['store'],'stock': i['stock'], 'price':i['price']}
-		list.append(dict)
-	return { "data" : list }
+  df = pd.read_csv("test.csv")
+  tableDataDf = df.loc[df['name'] == searchInput]
+  tableDataDf = tableDataDf.drop_duplicates(subset=['id'], keep='last')
+  result = tableDataDf.to_json(orient="table")
+  parsed = json.loads(result)
+  for i in parsed["data"]:
+    dict = {'name': i["name"], 'id': i['id'], 'store':i['store'],'stock': i['stock'], 'price':i['price']}
+    list.append(dict)
+  return { "data" : list }
 
 @app.route("/graph/<productID>")
 def getGraphData(productID):
-	df = pd.read_csv("test.csv")
-	graphdDf = df.loc[df['id'] == productID]
-	graphdDf = graphdDf.tail(5) # 5 most recent prices 
-	result = graphdDf.to_json(orient="table")
-	parsed = json.loads(result)
-	for i in parsed["data"]:
-		dict = {'id': i["id"], 'date': i['date'], 'price':i['price']}
-		list.append(dict)
-	return { "data" : list }
+  df = pd.read_csv("test.csv")
+  graphdDf = df.loc[df['id'] == productID]
+  graphdDf = graphdDf.tail(5) # 5 most recent prices 
+  result = graphdDf.to_json(orient="table")
+  parsed = json.loads(result)
+  for i in parsed["data"]:
+    dict = {'id': i["id"], 'date': i['date'], 'price':i['price']}
+    list.append(dict)
+  return { "data" : list }
 
 @app.route("/email/<emailInput>")
 def postEmail(email):
