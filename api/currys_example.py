9c413ae47cca046c6a3e06492403f38ace6512d5
Vincent <vincent.tn.le@gmail.com>
2021-06-25 19:41:09 -0400

    added product links

--- api/currys_example.py
+++ api/currys_example.py
@@ -3,16 +3,21 @@ from bs4 import BeautifulSoup
 from csv import DictWriter
 from datetime import date
 
-urls = [
-  "https://www.currys.com/product.htm?Product=SK50200&Source=Category&Category=SAKURA_PIGMA_SENSEI_MARKERS",
-  "https://www.currys.com/product.htm?Product=MUNG12&Source=Category&Category=MUNGYO_OIL_PASTEL_SETS",
-  "https://www.currys.com/catalogpc.htm?Category=CHARCOAL_PENCIL_SET_OF_4"
-]
 
 idDict = {
   "https://www.currys.com/product.htm?Product=SK50200&Source=Category&Category=SAKURA_PIGMA_SENSEI_MARKERS": 100,
   "https://www.currys.com/product.htm?Product=MUNG12&Source=Category&Category=MUNGYO_OIL_PASTEL_SETS": 101,
-  "https://www.currys.com/catalogpc.htm?Category=CHARCOAL_PENCIL_SET_OF_4": 102
+  "https://www.currys.com/catalogpc.htm?Category=CHARCOAL_PENCIL_SET_OF_4": 102,
+  "https://www.currys.com/catalogpc.htm?Category=WINSOR_NEWTON_FINELINER&NBReset=4":103,
+  "https://www.currys.com/product.htm?Product=SVC16202PK&Source=Category&Category=CURRYS_SUPER_VALUE_CANVAS_PACK":104,
+  "https://www.currys.com/product.htm?Product=VC06062PK&Source=Category&Category=VALUE_CANVAS_PACK_REGULAR":105,
+  "https://www.currys.com/product.htm?Product=MADC10&Source=Category&Category=BLACKBOARD_CHALK":106,
+  "https://www.currys.com/product.htm?Product=MUNG12&Source=Category&Category=MUNGYO_OIL_PASTEL_SETS":107,
+  "https://www.currys.com/product.htm?Product=FA8683&Source=Category&Category=KOHINOOR_CHARCOAL_BLOCKS_6PACK":108,
+  "https://www.currys.com/product.htm?Product=ACHGR&Source=Category&Category=ART_CHUNKY_CHARCOAL":109,
+  "https://www.currys.com/product.htm?Product=WN0290068&Source=Category&Category=WINSOR_NEWTON_FINELINER":110,
+  "https://www.currys.com/product.htm?Product=HGD410&Source=Category&Category=HOLBEIN_ACRYLA_GOUACHE":111,
+  "https://www.currys.com/product.htm?Product=GRM11180&Source=Category&Category=M_GRAHAM_ARTISTS_OIL_PAINT":112
 }
 
 def availability(soup):
@@ -22,27 +27,6 @@ def availability(soup):
   else:
     return "Available Online"
 
-"""
-## Writing to static csv.
-
-with open("scraped.csv", "a+", newline="") as csvFile:
-  fieldnames = ["productId", "storeName", "brandName", "itemName", "price", "availability", "dateToday"]
-  writer = DictWriter(csvFile, fieldnames=fieldnames)
-
-  for url in urls:
-    page = urlopen(url)
-    html = page.read().decode("utf-8")
-    soup = BeautifulSoup(html, "html.parser")
-    temp = {}
-    temp["productID"] = idDict[url]
-    temp["storeName"] = "Curry"s"
-    temp["brandName"] = soup.find("td", {"class": "PCContentDesc"}).text.split()[0]
-    temp["itemName"] = " ".join(soup.find("td", {"class": "PCContentDesc"}).text.split()[1:])
-    temp["price"] = soup.find_all("td", {"class": "PCContentYourPrc"})[1].text
-    temp["availability"] = availability(soup)
-    temp["dateToday"] = date.today()
-    writer.writerow(temp)
-"""
 
 import os
 import psycopg2 as db
@@ -57,23 +41,27 @@ cur = conn.cursor()
 
 context = ssl._create_unverified_context()
 
-for url in urls:
+for url in list(idDict.keys()):
   page = urlopen(url, context=context)
   html = page.read().decode("utf-8")
   soup = BeautifulSoup(html, "html.parser")
 
   productID = idDict[url]
+  productLink = url
   storeName = "Curry's"
   brandName = soup.find("td", {"class": "PCContentDesc"}).text.split()[0]
   itemName = " ".join(soup.find("td", {"class": "PCContentDesc"}).text.split()[1:])
   price = float(soup.find_all("td", {"class": "PCContentYourPrc"})[1].text.strip("$"))
   stock = availability(soup)
   dateToday = date.today()
-  cur.execute("SELECT DISTINCT * from products WHERE productID = {} and date = (select max(date) from products where productID = {})".format(productID,productID))
+  cur.execute("SELECT DISTINCT * from products WHERE productID = {} and dateToday = (select max(dateToday) from products where productID = {})".format(productID,productID))
   rows = cur.fetchall()
-
-  newSale = "Yes" if price < rows[0][4] else "No"
-  cur.execute("INSERT INTO products(productID, storeName, brandName, itemName, price, stock, dateToday, newSale) VALUES (%s, %s, %s, %s, %s, %s, %s,%s)", (productID, storeName, brandName, itemName, price, stock, dateToday, newSale))
+  
+  try:
+    newSale = "Yes" if price < rows[0][4] else "No"
+  except:
+    newSale = "No"
+  cur.execute("INSERT INTO products(productID, storeName, brandName, itemName, price, stock, dateToday, newSale, productLink) VALUES (%s, %s, %s, %s, %s, %s, %s,%s)", (productID, storeName, brandName, itemName, price, stock, dateToday, newSale, productLink))
   conn.commit()
 
 cur.close()
--- api/deserres_example.py
+++ api/deserres_example.py
@@ -3,21 +3,28 @@ from bs4 import BeautifulSoup
 from csv import DictWriter
 from datetime import date
 
-urls = [
-  "https://www.deserres.ca/en/12-pack-picolo-fineliners-0-4-mm", # Available
-  "https://www.deserres.ca/en/set-of-120-polychromos-colour-pencils-in-wood-case", # Available on sale
-  "https://www.deserres.ca/en/golden-acrylic-explorer-14-piece-set", # Available
-  "https://www.deserres.ca/products/box-set-of-50-extra-soft-pastels?variant=39427443294341", # Sold out
-  "https://www.deserres.ca/products/oval-paintbrush-with-a-short-handle?variant=39362011758725"
-]
+
 
 idDict = {
   "https://www.deserres.ca/en/12-pack-picolo-fineliners-0-4-mm": 200,
   "https://www.deserres.ca/en/set-of-120-polychromos-colour-pencils-in-wood-case": 201,
   "https://www.deserres.ca/en/golden-acrylic-explorer-14-piece-set": 202,
-  "https://www.deserres.ca/en/xl-pad-marker-9x12-70g-100s": 203,
   "https://www.deserres.ca/products/box-set-of-50-extra-soft-pastels?variant=39427443294341": 204,
-  "https://www.deserres.ca/products/oval-paintbrush-with-a-short-handle?variant=39362011758725": 205
+  "https://www.deserres.ca/products/oval-paintbrush-with-a-short-handle?variant=39362011758725": 205,
+  "https://www.deserres.ca/products/6-pack-copic-sketch-markers-skin-tones-1?variant=39427441098885":206,
+  "https://www.deserres.ca/products/12-pack-pitt-pastel-pencils?variant=39426411790469":207,
+  "https://www.deserres.ca/products/castell-9000-praphite-designer-pencils-set-of-12?variant=39426408218757":208,
+  "https://www.deserres.ca/products/set-of-24-lumograph-graphite-pencils?variant=39426425225349":209,
+  "https://www.deserres.ca/products/boar-bristled-oval-paintbrush?variant=39362519990405":210,
+  "https://www.deserres.ca/products/36-pack-copic-sketch-markers-basic?variant=39426416935045":211,
+  "https://www.deserres.ca/products/copic-ciao-markers-12-piece-basic-set?variant=39426416705669":212,
+  "https://www.deserres.ca/products/copic-sketch-markers-set-of-6-perfect-primary-colours?variant=39426417164421": 213,
+  "https://www.deserres.ca/products/cotman-watercolor-sketchers-pocket-box?variant=39426304344197":215,
+  "https://www.deserres.ca/products/24-colour-watercolour-set?variant=39426298577029":216,
+  "https://www.deserres.ca/products/32-piece-pwc-extra-fine-watercolour-set?variant=39426656108677":217,
+  "https://www.deserres.ca/products/studio-xl-oil-paint-set-24-x-20-ml?variant=39426298806405":221,
+  "https://www.deserres.ca/products/essentials-oil-paint-set-24-x-12-ml?variant=39427398205573":222
+
 }
 
 import os
@@ -33,24 +40,37 @@ cur = conn.cursor()
 
 context = ssl._create_unverified_context()
 
-for url in urls:
+for url in list(idDict.keys()):
   page = urlopen(url, context=context)
   html = page.read().decode("utf-8")
   soup = BeautifulSoup(html, "html.parser")
 
   productId = idDict[url]
+  productLink = url
   storeName = "Deserres"
   brandName = soup.find("a", {"class": "product__vendor-link"}).text.strip("\n")
   itemName = soup.find("h1", {"class": "product__title h3"}).text.strip("\n")
-  price = float(soup.find("span", {"class": "price__value price__value--final"}).text.strip("\n").strip("$"))
+  #If there is a sale then the page will have a slightly diff structure for price
+  print(itemName)
+  try:
+    price = float(soup.find("span", {"class":"price price--final"}).text.strip("\n").strip("$"))
+    print('sale price')
+  except:
+    price = float(soup.find("span", {"class": "price__value price__value--final"}).text.strip("\n").strip("$"))
+
+  print(price)
   stock = soup.find("span", {"class": "availability__label availability__label--in-stock"}).text
   dateToday = date.today()
 
-  cur.execute("SELECT DISTINCT * from products WHERE productID = {} and date = (select max(date) from products where productID = {})".format(productId, productId))
+  cur.execute("SELECT DISTINCT * from products WHERE productID = {} and dateToday = (select max(dateToday) from products where productID = {})".format(productId, productId))
   rows = cur.fetchall()
 
-  newSale = "Yes" if price < rows[0][4] else "No"
-  cur.execute("INSERT INTO products(productID, storeName, brandName, itemName, price, stock, dateToday, newSale) VALUES (%s, %s, %s, %s, %s, %s, %s,%s)", (productID, storeName, brandName, itemName, price, stock, dateToday, newSale))
+  #If the product is newly added (no previous record)
+  try:
+    newSale = "Yes" if price < rows[0][4] else "No"
+  except:
+    newSale = "No"
+  cur.execute("INSERT INTO products(productID, storeName, brandName, itemName, price, stock, dateToday, newSale, productLink) VALUES (%s, %s, %s, %s, %s, %s, %s,%s)", (productId, storeName, brandName, itemName, price, stock, dateToday, newSale, productLink))
   conn.commit()
 
 cur.close()
