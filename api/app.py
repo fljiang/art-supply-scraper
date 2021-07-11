c26c6f188e1f799a52f51389f09040623eb4cf19
Vincent <vincent.tn.le@gmail.com>
2021-07-11 15:49:36 -0400

    fix

--- api/app.py
+++ api/app.py
@@ -49,9 +49,9 @@ def getTableData(searchInput):
     returnVal["store"] = storeList
     returnVal["stock"] = stockList
     returnVal["price"] = priceList
-    return {"Data": returnVal}
+    return [{"Data": returnVal}]
   except: # Couldnt find product
-    return {"Data": ["could not find product"]}
+    return [{"Data": ["could not find product"]}]
 
 @app.route("/graph/<productId>")
 def getGraphData(productId):
