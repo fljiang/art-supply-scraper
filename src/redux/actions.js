330ad210e357d1d659b85473c909eb961d126bff
Felicia Jiang <46611531+fljiang@users.noreply.github.com>
2022-02-07 16:16:51 -0500

    Update actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,19 +2,19 @@ export const setSearchInput = (searchInput) => {
   return {
     type: "SET_SEARCH_INPUT",
     searchInput
-  }
-}
+  };
+};
 
 export const setProductId = (productId) => {
   return {
     type: "SET_PRODUCT_ID",
     productId
-  }
-}
+  };
+};
 
 export const setEmailInput = (emailInput) => {
   return {
     type: "SET_EMAIL_INPUT",
     emailInput
-  }
-}
+  };
+};
